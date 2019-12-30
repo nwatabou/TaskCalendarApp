3c0f471fc7313bbde02c991a8dfebb7463590fb2
nwatabou <wataru1.11@gmail.com>
2019-12-30 20:10:30 +0900

    必要なUI部品を配置

---
+++ TaskCalendarApp/View/CalendarDayCell.swift
@@ -0,0 +1,19 @@
+//
+//  CalendarDayCell.swift
+//  TaskCalendarApp
+//
+//  Created by nakanishi wataru on 2019/12/30.
+//  Copyright © 2019 nakanishi wataru. All rights reserved.
+//
+
+import UIKit
+
+class CalendarDayCell: UICollectionViewCell {
+
+    @IBOutlet weak var dayLabel: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
--- TaskCalendarApp/ViewController/CalendarViewController.swift
+++ TaskCalendarApp/ViewController/CalendarViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class CalendarViewController: UIViewController {
 
+    @IBOutlet weak var controllView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var calendarView: UICollectionView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
