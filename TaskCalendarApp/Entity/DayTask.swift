c1fb8cf4e8c3b3ebd1eabf255a2ccd2e39cf1ebb
nwatabou <wataru1.11@gmail.com>
2019-12-30 20:26:11 +0900

    Entityを作成

---
+++ TaskCalendarApp/Entity/DayTask.swift
@@ -0,0 +1,13 @@
+//
+//  DayTask.swift
+//  TaskCalendarApp
+//
+//  Created by nakanishi wataru on 2019/12/30.
+//  Copyright © 2019 nakanishi wataru. All rights reserved.
+//
+
+import Foundation
+
+struct DayTask {
+    var tasks: [Task]
+}
