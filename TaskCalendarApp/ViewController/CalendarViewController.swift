b4b0bbc3de0857f56a38daca8e5a6a5621ac3476
nwatabou <wataru1.11@gmail.com>
2019-12-30 19:49:01 +0900

    フィアル構成を修正

--- TaskCalendarApp/ViewController/CalendarViewController.swift
+++ TaskCalendarApp/ViewController/CalendarViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CalendarViewController.swift
 //  TaskCalendarApp
 //
 //  Created by nakanishi wataru on 2019/12/30.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CalendarViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
